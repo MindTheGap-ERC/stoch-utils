93afeb32d43c78a1925d9270e0e0b4b60ba4c300
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-08-04 10:14:58 +0200

    + OU and stationary OU process

---
+++ src/utils.R
@@ -0,0 +1,57 @@
+OU = function(from, to, mu, theta, sigma, x0, by = ((to - from)/(length.out - 1)), length.out = NULL){
+  #'
+  #'@title Simulate Ornstein-Uhlenbeck (OU) process
+  #'
+  #'@param from: numeric
+  #'@param to: numeric
+  #'@param mu: numeric
+  #'@param theta: numeric
+  #'@param x0: numeric
+  #'@param by: numeric
+  #'@param length.out: int
+  #'
+  #'@returns a list with components x and y. x contains where the OU is evaluated, y the values of the OU process
+  #'
+  #'@details simulation via EUler-Maruyama method, see 
+  #' https://en.wikipedia.org/w/index.php?title=Euler%E2%80%93Maruyama_method&oldid=1135099937
+  
+  
+  x = seq(from, to, by)
+  noiseIncrements <- rnorm(
+    n = length(x) - 1,
+    mean = 0,
+    sd = sqrt(by)
+  )
+  ouval <- rep(NA, length(x))
+  ouval[1] <- x0
+  for (j in 2:length(x)) {
+    ouval[j] <- ouval[j - 1] + by * (theta * (mu - ouval[j - 1])) +
+      sigma * noiseIncrements[j - 1]
+  }
+  return(list(x = x,
+              y = ouval))
+  
+}
+
+OUstat = function(from, to, mu, theta, sigma, by = ((to - from)/(length.out - 1)), length.out = NULL){
+  #'
+  #'@title Simulate stationary Ornstein-Uhlenbeck (OU) process
+  #'
+  #'@param from: numeric
+  #'@param to: numeric
+  #'@param mu: numeric
+  #'@param theta: numeric
+  #'@param x0: numeric
+  #'@param by: numeric
+  #'@param length.out: int
+  #'
+  #'@returns a list with components x and y. x contains where the OU is evaluated, y the values of the OU process
+  #'
+  #'@details simulation via EUler-Maruyama method, see 
+  #' https://en.wikipedia.org/w/index.php?title=Euler%E2%80%93Maruyama_method&oldid=1135099937
+  #' initial value x0 is chosen to make the process stationary
+  x0 = rnorm(1,
+             mean = mu,
+             sd = sigma / sqrt( 2 * theta))
+  return(OU(from, to, mu, theta, sigma, x0, by = ((to - from)/(length.out - 1)), length.out = NULL))
+}
