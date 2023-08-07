5ed4cc6eee8ffe502f51633962e838903db9fbe3
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-08-07 13:13:12 +0200

    bugfix

--- src/utils.R
+++ src/utils.R
@@ -50,10 +50,23 @@ OUstat = function(from, to, mu, theta, sigma, by = ((to - from)/(length.out - 1)
   #'@details simulation via EUler-Maruyama method, see 
   #' https://en.wikipedia.org/w/index.php?title=Euler%E2%80%93Maruyama_method&oldid=1135099937
   #' initial value x0 is chosen to make the process stationary
+  x = seq(from, to, by)
+  noiseIncrements <- rnorm(
+    n = length(x) - 1,
+    mean = 0,
+    sd = sqrt(by)
+  )
   x0 = rnorm(1,
              mean = mu,
              sd = sigma / sqrt( 2 * theta))
-  return(OU(from, to, mu, theta, sigma, x0, by = ((to - from)/(length.out - 1)), length.out = NULL))
+  ouval <- rep(NA, length(x))
+  ouval[1] <- x0
+  for (j in 2:length(x)) {
+    ouval[j] <- ouval[j - 1] + by * (theta * (mu - ouval[j - 1])) +
+      sigma * noiseIncrements[j - 1]
+  }
+  return(list(x = x,
+              y = ouval))
 }
 
 BM = function(x, y0 = 0){
